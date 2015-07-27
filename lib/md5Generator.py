fa2e39cfa1afcb9e49f6aead9609c52eafb8e794
shiva2020 <dev-team@rooksecurity.com>
2015-07-27 13:53:31 -0900

    Experimenting with open calls

--- lib/md5Generator.py
+++ lib/md5Generator.py
@@ -9,3 +9,13 @@ class Md5Generator(object):
                 m.update(chunk)
 
         return m.hexdigest()
+
+#class Md5Generator(object):
+#    def compute_md5(self, file_path):
+#        f = open(file_path, "rb+")
+#
+#        m = hashlib.md5()
+#        for chunk in iter(lambda: f.read(hashlib.md5().block_size * 128), b''):
+#            m.update(chunk)
+#
+#        f.close()
