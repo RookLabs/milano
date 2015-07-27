690f113b8709bd9bee6ed3a6b79601f1fdfadd03
shiva2020 <dev-team@rooksecurity.com>
2015-07-27 05:28:35 -0900

    Updated md5 compute function

--- lib/md5Generator.py
+++ lib/md5Generator.py
@@ -2,11 +2,10 @@ import hashlib
 from functools import partial
 
 class Md5Generator(object):
-	def compute_md5(self, file_path):
-		chunksize=2**15
-		bufsize=-1
-		m = hashlib.md5()
-		with open(file_path, 'rb', bufsize) as f:
-			for chunk in iter(partial(f.read, chunksize), b''):
-				m.update(chunk)
-		return m.hexdigest()
+    def compute_md5(self, file_path):
+        m = hashlib.md5()
+        with open(file_path, 'rb', buffering=-1) as f:
+            for chunk in iter(lambda: f.read(hashlib.md5().block_size * 128), b''):
+                m.update(chunk)
+
+        return m.hexdigest()
