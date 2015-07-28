eb2ae3c6b0d8ad7dd0339e392e8ac910f626f297
shiva2020 <dev-team@rooksecurity.com>
2015-07-28 04:28:13 -0900

    Fixing file open

--- lib/md5Generator.py
+++ lib/md5Generator.py
@@ -1,21 +1,16 @@
+import os
+import stat
 import hashlib
-from functools import partial
 
 class Md5Generator(object):
     def compute_md5(self, file_path):
         m = hashlib.md5()
-        with open(file_path, 'rb', buffering=-1) as f:
+        with os.fdopen(os.open(file_path, (os.O_RDONLY | os.O_NONBLOCK | os.O_NOFOLLOW)), 'rb', 0) as f:
             for chunk in iter(lambda: f.read(hashlib.md5().block_size * 128), b''):
                 m.update(chunk)
 
         return m.hexdigest()
 
-#class Md5Generator(object):
-#    def compute_md5(self, file_path):
-#        f = open(file_path, "rb+")
-#
-#        m = hashlib.md5()
-#        for chunk in iter(lambda: f.read(hashlib.md5().block_size * 128), b''):
-#            m.update(chunk)
-#
-#        f.close()
+if __name__ == "__main__":
+    g = Md5Generator()
+    print g.compute_md5('/dev/urandom')
