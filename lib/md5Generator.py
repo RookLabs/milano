f5fd37946e5c7f6ba2aebbc0a3f8a9d0f653c6da
shiva2020 <dev-team@rooksecurity.com>
2015-07-28 04:44:27 -0900

    Fixing file scanning on Linux

--- lib/md5Generator.py
+++ lib/md5Generator.py
@@ -5,12 +5,13 @@ import hashlib
 class Md5Generator(object):
     def compute_md5(self, file_path):
         m = hashlib.md5()
-        with os.fdopen(os.open(file_path, (os.O_RDONLY | os.O_NONBLOCK | os.O_NOFOLLOW)), 'rb', 0) as f:
-            for chunk in iter(lambda: f.read(hashlib.md5().block_size * 128), b''):
+        with os.fdopen(os.open(file_path, (os.O_RDONLY | os.O_NONBLOCK | os.O_NOFOLLOW))) as f:
+            for chunk in iter(lambda: f.read(hashlib.md5().block_size * 512), b''):
                 m.update(chunk)
 
         return m.hexdigest()
 
 if __name__ == "__main__":
     g = Md5Generator()
-    print g.compute_md5('/dev/urandom')
+    print '/dev/urandom md5 -> {}'.format(g.compute_md5('/dev/urandom'))
+    print '/boot/vmlinuz-3.16.0-43-generic md5 -> {}'.format(g.compute_md5('/boot/vmlinuz-3.16.0-43-generic'))
